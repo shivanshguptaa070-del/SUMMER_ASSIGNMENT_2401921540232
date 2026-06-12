2c9dddec8efc756e6dd574c9502536bdf1c47e7a
Shivansh Gupta <shivanshguptaa070@gmail.com>
2026-06-12 21:38:42 +0530

    Update Group_Anagrams.py

--- WEEK 2/DAY 5/Group_Anagrams.py
+++ WEEK 2/DAY 5/Group_Anagrams.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
         d = {}
-
         for s in strs:
             key = "".join(sorted(s))
-
             if key not in d:
                 d[key] = []
-
             d[key].append(s)
-
         return list(d.values())
