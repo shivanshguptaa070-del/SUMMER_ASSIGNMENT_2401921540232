32e080e428f9e46f8c128ca55911a0da607656b0
Shivansh Gupta <shivanshguptaa070@gmail.com>
2026-06-12 21:38:35 +0530

    Update String_Compression.py

--- WEEK 2/DAY 5/String_Compression.py
+++ WEEK 2/DAY 5/String_Compression.py
@@ -4,25 +4,19 @@ class Solution(object):
         :type chars: List[str]
         :rtype: int
         """
-        
         i = 0
         w = 0
-
         while i < len(chars):
             ch = chars[i]
             cnt = 0
-
             while i < len(chars) and chars[i] == ch:
                 cnt += 1
                 i += 1
-
             chars[w] = ch
             w += 1
-
             if cnt > 1:
                 for c in str(cnt):
                     chars[w] = c
                     w += 1
-
         return w
         
