e6ca666131944eb70343cf4819db0e0c802a6ad2
Shivansh Gupta <shivanshguptaa070@gmail.com>
2026-06-16 23:51:18 +0530

    update and qadd all these things

---
+++ WEEK 3/DAY 2/Palindrome_Linked_List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+        return arr == arr[::-1]
