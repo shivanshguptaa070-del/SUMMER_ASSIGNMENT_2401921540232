493245cff12224af2c266fe11572459284ae06fd
Shivansh Gupta <shivanshguptaa070@gmail.com>
2026-06-25 06:45:22 +0530

    DAY 3

---
+++ WEEK 4/DAY 3/SearchinaBinarySearchTree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        curr=root
+        while curr and curr.val!=val:
+            if val<curr.val:
+                curr=curr.left
+            else:
+                curr=curr.right
+        return curr
