26404a425ca72eedaedff5675fc014da278b6e00
Shivansh Gupta <shivanshguptaa070@gmail.com>
2026-06-28 05:58:53 +0530

    Update Serialize and Deserialize Binary Tree.py

--- WEEK 4/DAY 5/Serialize and Deserialize Binary Tree.py
+++ WEEK 4/DAY 5/Serialize and Deserialize Binary Tree.py
@@ -1,3 +1,32 @@
-# Problem: Serialize and Deserialize Binary Tree
-# Link: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+class Codec:
+    def serialize(self, root):
+        if not root:
+            return ""
+        result = []
+        def dfs(node):
+            if not node:
+                result.append("N")
+                return
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(result)
 
+    def deserialize(self, data):
+        if not data:
+            return None
+        vals = data.split(",")
+        self.i = 0
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        
+        return dfs()
